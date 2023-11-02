7f930745c2bfeee8cb74480340f72ad10de8ae87
Zhuo0420 <86188276+Zhuo0420@users.noreply.github.com>
2023-11-03 00:19:26 +0800

    Add files via upload

---
+++ 11286 - Conformity.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <map>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	map<string, int> course;			//存組合 <組合字串, 有幾個學生有種些組合>
+	vector<string> class_num;			//class number
+	string line;
+
+	while (cin >> n && n != 0) {
+		int max_num = 0;
+		int total = 0;
+		course.clear();
+
+		while (n--) {
+			line.clear();
+			class_num.clear();
+			for (int i = 0; i < 5; i++) {
+				string tmp;
+				cin >> tmp;
+				class_num.push_back(tmp);
+			}
+			sort(class_num.begin(), class_num.end());
+			for (int i = 0; i < 5; i++) {
+				line += class_num[i];
+			}
+			course[line]++;
+		}
+
+		for (auto it = course.begin(); it != course.end(); ++it)		//找最多人選的組合，max_num為有此組合的學生數
+			if (it->second > max_num) max_num = it->second;
+		for (auto it = course.begin(); it != course.end(); ++it)
+			if (it->second == max_num) total += max_num;
+
+		cout << total << endl;
+		
+	}
+
+	return 0;
+}
---
+++ 978 - Lemmings Battle!.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    while (n--) {
+        int B, SG, SB;
+        multiset <int> green;
+        multiset <int> blue;
+        vector<int> result;
+        cin >> B >> SG >> SB;
+        for (int i = 0; i < SG; i++) {
+            int tmp;
+            cin >> tmp;
+            green.emplace(tmp);
+        }
+        for (int i = 0; i < SB; i++) {
+            int tmp;
+            cin >> tmp;
+            blue.emplace(tmp);
+        }      
+        while (!(green.empty()) && !(blue.empty())) {
+            for (int i = 0; i < B; i++) {
+                if (green.empty() || blue.empty())  break;
+                result.emplace_back(*green.rbegin() - *blue.rbegin());
+                auto it = green.end();
+                green.erase(--it);
+                it = blue.end();
+                blue.erase(--it);
+            }
+            for (auto i : result) {
+                if (i > 0) green.emplace(i);
+                else if (i < 0)    blue.emplace(abs(i));
+            }
+            result.clear();            
+        }    
+        multiset<int>::reverse_iterator it;
+        if (!(green.empty()) && blue.empty()) {
+            cout << "green wins" << endl;            
+            for (it = green.rbegin(); it != green.rend();it++) {
+                cout << *it << endl;
+            }
+        }
+        else if (green.empty() && !(blue.empty())) {
+            cout << "blue wins" << endl;           
+            for (it = blue.rbegin(); it != blue.rend(); it++) {
+                cout << *it << endl;
+            }
+        }
+        else {
+            cout << "green and blue died" << endl;
+        }      
+        if(n!=0)
+            cout << endl;
+    }
+}
