1953ceec49840ad9ef1ac59d1a8382c7ed9b7e09
Zhuo0420 <86188276+Zhuo0420@users.noreply.github.com>
2023-11-03 23:00:43 +0800

    Add files via upload

---
+++ 11136 - Hoax or what.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main()
+{
+    int n;      //days    
+    while (cin >> n) {
+        if (n == 0)    break;
+        long long int k;      //bills
+        multiset<long long int> pri;
+        long long int sum = 0;
+        while (n--){
+            cin >> k;                   
+            for (int i = 0; i < k; i++) {
+                long long int temp;
+                cin >> temp;
+                pri.insert(temp);
+            }
+            if (!(pri.empty())) {
+                sum += *(--pri.end()) - *pri.begin();
+                pri.erase(pri.begin());
+                if (!(pri.empty()))     pri.erase(--(pri.end()));
+            }
+        }
+        cout << sum << endl;        
+    }
+}
