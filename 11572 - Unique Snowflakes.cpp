08ae9fa48cb3b0444140561408a748c43915cc87
Zhuo0420 <86188276+Zhuo0420@users.noreply.github.com>
2023-11-04 01:28:20 +0800

    Add files via upload

---
+++ 11572 - Unique Snowflakes.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    while (cases--) {
+        int n, maxLength = 0;
+        cin >> n;        
+        unordered_set<int> s;
+        vector<int> v;
+        for (int i = 0; i < n; i++) {
+            int tmp;
+            cin >> tmp;
+            if (s.count(tmp))   if (maxLength < s.size())  maxLength = s.size();                
+            while (s.count(tmp)) s.erase(*(v.begin())), v.erase(v.begin());
+
+            s.emplace(tmp);
+            v.emplace_back(tmp);
+        }
+        cout << maxLength << endl;
+    }
+}
