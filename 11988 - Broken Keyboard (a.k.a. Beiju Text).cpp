715908c3ce59663e925b9610afabdd6a92e19b8d
Zhuo0420 <86188276+Zhuo0420@users.noreply.github.com>
2023-11-07 01:39:54 +0800

    Add files via upload

---
+++ 11988 - Broken Keyboard (a.k.a. Beiju Text).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <deque>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    bool n = 0;
+    while (getline(cin, str)) {
+        deque<string> dqu;
+        string tmp = "";
+        n = 1;      //true => i 在 [] 外, false => i 在 [] 內   
+        for (int i = 0; i < str.length(); i++) {
+            if (str[i] == '[' || str[i] == ']') {
+                if (n) dqu.push_back(tmp);
+                else    dqu.push_front(tmp);
+                n = (str[i] == ']');
+                tmp = "";
+            }
+            else    tmp += str[i];
+        }
+        if (n) dqu.push_back(tmp);
+        else    dqu.push_front(tmp);
+        for (auto i : dqu) {
+            cout << i;
+        }
+        cout << endl;
+    }
+}
