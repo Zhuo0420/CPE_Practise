32027f59eaab56099e1fe2a012565949238ca6aa
Zhuo0420 <86188276+Zhuo0420@users.noreply.github.com>
2023-11-11 15:38:21 +0800

    Add files via upload

---
+++ 1203 - Argus.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <queue>
+#include <string>
+#include <tuple>
+
+using namespace std;
+
+typedef tuple<int, int, int> datas;		//經過時間 que編號 頻率
+
+int main()
+{
+	string str;
+	priority_queue<datas, vector<datas>, greater<datas>> que;
+	int num, P, K;
+	while (cin >> str && str != "#") {	
+		cin >> num >> P;
+		que.emplace(make_tuple(P, num, P));		
+	}
+	cin >> K;
+	int t = 0;
+	while (K--) {
+		tie(t, num, P) = que.top();
+		que.pop();
+		que.emplace(make_tuple(P + t, num, P));
+		cout << num << endl;
+	}
+}
