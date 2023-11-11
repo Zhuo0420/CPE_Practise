da41482be849c6d1a8255fdecee0fbd50a338753
Zhuo0420 <86188276+Zhuo0420@users.noreply.github.com>
2023-11-12 02:24:09 +0800

    Add files via upload

---
+++ 11995 - I Can Guess the Data Structure!.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <queue>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	int n;
+	while (cin >> n) {
+		int input;
+		int com;
+		queue<int> lineQ;
+		stack<int> lineS;
+		priority_queue<int> linePQ;
+		bool Q = 1, S = 1, PQ = 1;
+		while (n--) {
+			cin >> com >> input;
+			if (com == 1) {				
+				lineQ.emplace(input);
+				lineS.emplace(input);
+				linePQ.emplace(input);
+			}
+			else{
+				int tmpQ, tmpS, tmpPQ;
+				if (!lineQ.empty())	tmpQ = lineQ.front();
+				if (!lineS.empty())	tmpS = lineS.top();
+				if (!linePQ.empty())	tmpPQ = linePQ.top();					
+				if (tmpQ) {
+					if (!lineQ.empty() && input == tmpQ)		lineQ.pop();
+					else	Q = false;
+				}				
+				if (tmpS) {
+					if (!lineS.empty() && input == tmpS)		lineS.pop();
+					else	S = false;
+				}
+				if (tmpPQ) {
+					if (!linePQ.empty() && input == tmpPQ)		linePQ.pop();
+					else	PQ = false;	
+				}
+				
+			}
+		}
+		if (Q && !S && !PQ)			cout << "queue" << endl;
+		else if (!Q && S && !PQ)	cout << "stack" << endl;
+		else if (!Q && !S && PQ)	cout << "priority queue" << endl;
+		else if (!Q && !S && !PQ)	cout << "impossible" << endl;
+		else	cout << "not sure" << endl;
+	}
+	return 0;
+}
