4e22443cdf1862490c355683dc36f911be80a07e
Zhuo0420 <86188276+Zhuo0420@users.noreply.github.com>
2023-11-19 01:38:03 +0800

    Update 11572 - Unique Snowflakes.cpp

--- 11572 - Unique Snowflakes.cpp
+++ 11572 - Unique Snowflakes.cpp
@@ -1,6 +1,8 @@
-﻿#include <iostream>
+#include <iostream>
 #include <unordered_set>
 #include <vector>
+
+
 using namespace std;
 
 int main()
@@ -8,19 +10,25 @@ int main()
     int cases;
     cin >> cases;
     while (cases--) {
-        int n, maxLength = 0;
-        cin >> n;        
+        int k;
+        int ans = 0;
         unordered_set<int> s;
-        vector<int> v;
-        for (int i = 0; i < n; i++) {
+        vector<int>v;
+        cin >> k;
+        while (k--) {
             int tmp;
             cin >> tmp;
-            if (s.count(tmp))   if (maxLength < s.size())  maxLength = s.size();                
-            while (s.count(tmp)) s.erase(*(v.begin())), v.erase(v.begin());
-
+            if (s.count(tmp)) {
+                if (ans <= s.size())    ans = s.size();
+            }
+            while (s.count(tmp)) {
+                s.erase(*v.begin());
+                v.erase(v.begin());
+            }
             s.emplace(tmp);
             v.emplace_back(tmp);
         }
-        cout << maxLength << endl;
+        if (ans <= s.size())    ans = s.size();
+        cout << ans << endl;
     }
 }
